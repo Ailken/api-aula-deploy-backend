c6220299bb81117d506036f997c763bce8152379
Ailken <ailkencf@gmail.com>
2023-10-25 10:53:19 -0300

    Implementação de conexão

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 const express = require('express');
+const knex = require('./conexao')
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    return res.json('Api esta ok!')
+
+    try {
+        const carros = await knex('carros')
+        return res.json(carros)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ mensagem: 'erro do servidor.'})
+    }
 });
 
 const port = process.env.PORT || 3000
